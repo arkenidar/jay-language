4b49a804dc09b124a616c20822fb4ff1644e0611
Dario "Arkenidar" Cangialosi <dario.cangialosi@gmail.com>
2023-02-10 21:32:32 +0100

    [execution context] steps toward better code reuse and dynamic "context" for execution (execution context)

--- hello.js
+++ hello.js
@@ -2,62 +2,57 @@
 
 // 8 keywords: "$func","$skip","$val","$expr","$op","$attr","$attr_set","$call"
 
-globalThis. $expr = function $expr(expr){
-    let index;
+globalThis. $expr = function $expr(expr, context){
+
     if(expr[0]!=="$expr"){
         console.error("mismatch in $expr :",expr[0])
         return null
     }
-    let ip = 1;
-    let context = globalThis;
+
     let current_value = null;
     let operation_id = null;
-    while(ip < expr.length){
+
+    for(let ip = 1; ip < expr.length; ip++){
         let current = expr[ip];
+
         let do_operation = false;
         let new_value = false;
         let previous_value = current_value;
 
         if(current[0]==="$attr"){
-            let attr = current;
-
-            for(index = 1; index<attr.length; index += 1) {
-                let attribute_id = attr[index];
-
-                if (typeof context[attribute_id] == "undefined") {
-                    console.error("undefined context[attribute_id] :", attribute_id)
-                    return undefined
-                }
-
-                current_value = context[attribute_id]
-                new_value = true
-
-                context = current_value
-            }
-        } else if(current[0]==="$attr_set"){
-            let attr_set = current;
-            current_value = context[attr_set[1]]=get(attr_set[2])
+            current_value = $attr(current, context)
             new_value = true
+
+        } else if(current[0]==="$attr_set"){
+            current_value = $attr_set(current, current_value, context)
+            new_value = true
+
         } else if(current[0]==="$op"){
             operation_id = current[1]
+
         } else if(current[0]==="$val"){
-            current_value = current[1]
+            current_value = $val(current)
             new_value = true
+
         } else if(current[0]==="$expr"){
-            current_value = $expr(current)
+            current_value = $expr(current, context)
             new_value = true
+
         } else if(current[0]==="$call"){
+
             let arguments = [];
-            for(index = 1; index<current.length; index += 1){
+            for(let index = 1; index<current.length; index += 1){
                 let argument = current[index];
-                arguments.push(get(argument))
+                arguments.push(get(argument, context))
             }
-            current_value = context(...arguments)
+            current_value = current_value(...arguments)
             new_value = true
+
         } else {
             console.error("unsupported in $expr :", current[0])
             return null
         }
+
         do_operation = new_value && operation_id!=null
         if(do_operation){
             if(operation_id==="+"){
@@ -76,50 +71,57 @@ globalThis. $expr = function $expr(expr){
             operation_id = null
         }
 
-        context = current_value
-
-        ip += 1
     }
     return current_value
-}
-
-globalThis. $attr_set= function $attr_set(attr_set){
-    return globalThis[attr_set[1]]=get(attr_set[2])
 }
 
 globalThis. $val= function $val(val){
     return val[1]
 }
 
-globalThis. $attr= function $attr(attr){
-    let context = globalThis;
-    let result;
+globalThis. $attr= function $attr(attr, context){
+    let current = context;
     for(let index = 1; index<attr.length; index += 1){
         let attribute_id = attr[index];
 
-        if(typeof context[attribute_id]=="undefined"){
-            console.error("undefined context[attribute_id] :", attribute_id)
+        if(typeof current[attribute_id]=="undefined"){
+            console.error("undefined current[attribute_id] :", attribute_id)
             return undefined
         }
-        result = context[attribute_id]
-
-        context = result
+        current = current[attribute_id]
     }
-    return result
+    return current
 }
 
-function get(json){
-    if(typeof globalThis[json[0]] == "function"){
-        return globalThis[json[0]](json)
+globalThis. $attr_set= function $attr_set(attr_set, of_object, context){
+    return of_object[attr_set[1]] = get(attr_set[2], context)
+}
+
+function get(json, context=globalThis){
+    if(typeof context[json[0]] == "function"){
+        return context[json[0]](json, context)
     } else {
         console.error("no function found for get() :",json[0])
         return null
     }
 }
 
-get(["$expr", ["$attr","console","log"], ["$call", ["$val", 123]]])
+function tests1(){
+    // outputs 123
+    get(["$expr", ["$attr","console","log"], ["$call", ["$val", 123]]])
 
-get(["$attr_set", "var_name", ["$val", {"counter":1}]])
-var_name.counter += 10
-get(["$expr",["$attr","var_name"],["$attr_set","counter",["$expr",["$attr","var_name","counter"],["$op","+"],["$val",10]]]])
-console.log(get(["$attr","var_name","counter"]))
+    // outputs 21
+    get(["$attr_set", "var_name", ["$val", {"counter":1}]])
+    var_name.counter += 10
+    get(["$expr",["$attr","var_name"],["$attr_set","counter",["$expr",["$attr","var_name","counter"],["$op","+"],["$val",10]]]])
+    console.log(get(["$attr","var_name","counter"]))
+}
+
+function tests2(){
+    // outputs *local1*
+    let local_context={...globalThis, "local_variable1":"*local1*"}
+    console.log( get(["$attr","local_variable1"], local_context) )
+}
+
+tests1()
+tests2()
